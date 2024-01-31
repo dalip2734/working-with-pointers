d9dd1b45c87655db537da94e45034dfdef4fc78d
Dalip Yadav <dalipyadav68@gmail.com>
2024-01-31 18:57:28 +0530

    DeclarationOfPointers

---
+++ lecture_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 6;
+    int *p;  // declaring a poitner variable p of type integer
+    p = &a;   // p = address of a 
+    cout<<p<<endl;  // address of a
+    cout<<a<<endl;  // value of a
+    *p = 5;   // value at address which is stored in p
+    cout<<*p<<endl;  // 6
+
+}
