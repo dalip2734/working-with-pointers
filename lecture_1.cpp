87a226918945e6f653276601afbd44676942bb21
Dalip Yadav <dalipyadav68@gmail.com>
2024-01-31 21:26:08 +0530

    Add Comments

--- lecture_1.cpp
+++ lecture_1.cpp
@@ -7,8 +7,8 @@ int main()
     int *p;  // declaring a poitner variable p of type integer
     p = &a;   // p = address of a 
     cout<<p<<endl;  // address of a
-    cout<<a<<endl;  // value of a
+    cout<<a<<endl;  // 6
     *p = 5;   // value at address which is stored in p
-    cout<<*p<<endl;  // 6
+    cout<<*p<<endl;  // 5
 
 }
