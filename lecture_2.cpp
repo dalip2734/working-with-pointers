9fb7e3518968a92730e37cb11fa110e323ae01ef
Dalip Yadav <dalipyadav68@gmail.com>
2024-02-04 18:03:47 +0530

    PointerArithmetic

---
+++ lecture_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for integer
+    int a;
+    int *p;
+    p = &a;
+    // for character
+    char c;
+    char *v;
+    v = &c;
+
+    // Pointer arithmatic
+
+    cout << "Address of p =" << p << endl;
+    cout << "size if integer is = " << sizeof(int) << endl;
+    cout << "Address p+1 is =" << p + 1 << endl;
+
+    // for chracter pointer
+    cout << "Address of v = " << v << endl;
+    cout << "size of char =" << sizeof(char) << endl;
+    cout << "Address of v+1 =" << v + 1 << endl;
+}
